d89deff545cc62c99aa0e8d5ef950c7df14d0aec
Leung Po Ming <>
2021-09-02 15:57:57 +0800

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'colors.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class Controller extends GetxController {
-  var count = 0.obs;
-
-  increment() {
-    count = count + 1;
-  }
-}
-
-class Page1 extends StatelessWidget {
+class DisplayListPage extends StatelessWidget {
   @override
-  Widget build(context) {
-    // Instantiate your class using Get.put() to make it available for all "child" routes there.
-    final Controller c = Get.put(Controller(), tag: 'Page1');
-
-    return Scaffold(
-        // Use Obx(()=> to update Text() whenever count is changed.
-        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
-
-        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
-        body: Center(
-            child: ElevatedButton(
-                child: Text("Go to Page2"), onPressed: () => Get.to(Page2()))),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add), onPressed: c.increment));
-  }
-}
-
-class Page2 extends StatelessWidget {
-  @override
-  Widget build(context) {
-    // Instantiate your class using Get.put() to make it available for all "child" routes there.
-    final Controller c = Get.put(Controller(), tag: 'Page2');
-
-    return Scaffold(
-        // Use Obx(()=> to update Text() whenever count is changed.
-        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
-
-        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
-        body: Center(
-            child: ElevatedButton(
-                child: Text("Go to Other"), onPressed: () => Get.to(Other()))),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add), onPressed: c.increment));
-  }
-}
-
-class Other extends StatelessWidget {
-  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
-  final Controller c = Get.find(tag: 'Page2');
-
-  @override
-  Widget build(context) {
-    // Access the updated count variable
-    return SafeArea(child: Scaffold(body: Center(child: Text("${c.count}"))));
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: [Text('Here is the DisplayListPage')],
+      ),
+    ));
   }
 }
 
@@ -68,9 +25,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
         title: 'Flutter Demo',
+        initialRoute: '/',
         theme: ThemeData(
-          primarySwatch: Colors.pink,
+          primaryColor: white,
         ),
-        home: SafeArea(child: Page1()));
+        getPages: [
+          GetPage(
+            name: '/',
+            page: () => DisplayListPage(),
+          )
+        ]);
   }
 }
