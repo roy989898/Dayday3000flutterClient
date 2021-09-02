545d877cc1e32d881542524dfc89fad3064b34d7
Leung Po Ming <>
2021-09-02 15:05:16 +0800

    use get x

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,51 @@ void main() {
   runApp(MyApp());
 }
 
+class Controller extends GetxController {
+  var count = 0.obs;
+
+  increment() => count++;
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(context) {
+    // Instantiate your class using Get.put() to make it available for all "child" routes there.
+    final Controller c = Get.put(Controller());
+
+    return Scaffold(
+        // Use Obx(()=> to update Text() whenever count is changed.
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+
+        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
+        body: Center(
+            child: ElevatedButton(
+                child: Text("Go to Other"), onPressed: () => Get.to(Other()))),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: c.increment));
+  }
+}
+
+class Other extends StatelessWidget {
+  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
+  final Controller c = Get.find();
+
+  @override
+  Widget build(context) {
+    // Access the updated count variable
+    return Scaffold(body: Center(child: Text("${c.count}")));
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      /*     appBar: AppBar(
-        title: Text(widget.title),
-      ),*/
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
+    return GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: Home());
   }
 }
