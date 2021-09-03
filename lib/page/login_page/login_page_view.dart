492a6fc7b0aa8cd908bb2451b4de91d0de4e535f
Leung Po Ming <>
2021-09-03 11:36:53 +0800

    add name pwd and login bt

--- lib/page/login_page/login_page_view.dart
+++ lib/page/login_page/login_page_view.dart
@@ -1,3 +1,4 @@
+import 'package:day_day_3000_fluter_client/statis.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,21 +12,28 @@ class LoginPagePage extends StatelessWidget {
 
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-              child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration.collapsed(hintText: 'Name'),
-              ),
-              TextField(
-                decoration: InputDecoration.collapsed(hintText: 'Password'),
-              ),
-            ],
-          ))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(Statis.fullPadding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration.collapsed(hintText: 'Name'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: Statis.padding4),
+                  child: TextField(
+                    decoration: InputDecoration.collapsed(hintText: 'Password'),
+                  ),
+                ),
+                TextButton(onPressed: () {}, child: Text('Login'))
+              ],
+            ))
+          ],
+        ),
       ),
     ));
   }
---
+++ lib/statis.dart
@@ -0,0 +1,5 @@
+class Statis {
+ static const fullPadding = 16.0;
+ static  const halfPadding = 8.0;
+ static  const padding4 = 8.0;
+}
