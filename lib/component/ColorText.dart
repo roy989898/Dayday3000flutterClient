20dffde45d6383a7b88175d3ff4ae15dbbf01052
Leung Po Ming <>
2021-09-03 13:20:26 +0800

    can make the title change color

--- lib/component/ColorText.dart
+++ lib/component/ColorText.dart
@@ -8,19 +8,32 @@ import '../statis.dart';
 class ColorText extends StatefulWidget {
   final String text;
 
-  const ColorText({Key? key, required this.text}) : super(key: key);
+  ColorText({Key? key, required this.text}) : super(key: key);
 
   @override
   _ColorTextState createState() => _ColorTextState();
 }
 
 class _ColorTextState extends State<ColorText> {
+  var _beginColor = TWTwoColors.yellow.shade500;
+  var _endColorColor = TWTwoColors.pink.shade500;
+
   @override
   Widget build(BuildContext context) {
     return TweenAnimationBuilder(
-        tween: ColorTween(
-            begin: TWTwoColors.yellow.shade500, end: TWTwoColors.pink.shade500),
+        tween: ColorTween(begin: _beginColor, end: _endColorColor),
         duration: Duration(seconds: 2),
+        onEnd: () {
+          setState(() {
+            if (_endColorColor == TWTwoColors.pink.shade500) {
+              _endColorColor = TWTwoColors.yellow.shade500;
+            } else {
+              _endColorColor = TWTwoColors.pink.shade500;
+            }
+
+            // _endColorColor = TWTwoColors.yellow.shade500;
+          });
+        },
         builder: (_, Color? color, __) {
           return Text(widget.text,
               style: GoogleFonts.dancingScript(
