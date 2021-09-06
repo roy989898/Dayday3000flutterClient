1730c5f6131c06f98af648e6827e9c418fbb6b3b
Leung Po Ming <>
2021-09-06 15:37:13 +0800

    add some theme data

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,24 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
         title: 'Flutter Demo',
         initialRoute: '/login',
-        theme: ThemeData(primarySwatch: Colors.pink),
+        theme: ThemeData(
+          //TODO make the theme
+          // Define the default brightness and colors.
+          brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue[800],
+          accentColor: Colors.cyan[600],
+
+          // Define the default font family.
+          fontFamily: 'Georgia',
+
+          // Define the default TextTheme. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: const TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),
+        ),
         getPages: [
           GetPage(
             name: '/',
