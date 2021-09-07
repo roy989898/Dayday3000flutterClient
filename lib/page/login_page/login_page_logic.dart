aa69ea3fbc7f378607e899afff24c451ad3456a9
Leung Po Ming <>
2021-09-07 16:03:56 +0800

    can get some data

--- lib/page/login_page/login_page_logic.dart
+++ lib/page/login_page/login_page_logic.dart
@@ -25,12 +25,27 @@ class LoginPageLogic extends GetxController {
     QuerySnapshot<Map<String, dynamic>> stockResult = await stock.get();
 
     //get the filed in a doc
-    var fDate=stockResult.docs.first.get('date');
+    var fDate = stockResult.docs.first.get('date');
     //get the collection in a doc
-    var fdId=stockResult.docs.first.id;
-    var stockConnectionInSpecificDate = firestore.collection('stock/$fdId/stock');
-    var stockConnectionInSpecificDateResult= await stockConnectionInSpecificDate.get();
-    var xx=stockConnectionInSpecificDateResult.docs.first.data();
+    var fdId = stockResult.docs.first.id;
+    var stockConnectionInSpecificDate =
+        firestore.collection('stock/$fdId/stock');
+    var stockConnectionInSpecificDateResult =
+        await stockConnectionInSpecificDate.get();
+    var xx = stockConnectionInSpecificDateResult.docs.first.get('current_lower_price');
     var i = 1 + 1;
+
+    stockResult.docs.forEach((doc) async {
+      // String date = doc.get('date');
+      var docId = doc.id;
+      var pathOfStockDoc = 'stock/$docId/stock';
+      var stockConnectionInSpecificDate = firestore.collection(pathOfStockDoc);
+      var stockConnectionInSpecificDateResult =
+          await stockConnectionInSpecificDate.get();
+      stockConnectionInSpecificDateResult.docs.forEach((aStock) {
+        var current_lower_price = aStock.get('current_lower_price');
+        var i = 1 + 1;
+      });
+    });
   }
 }
