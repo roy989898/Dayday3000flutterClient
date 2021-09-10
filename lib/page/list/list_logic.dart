1e6d23e68f924972660f34c76b8c95d9505414e4
Leung Po Ming <>
2021-09-10 16:28:14 +0800

    save

--- lib/page/list/list_logic.dart
+++ lib/page/list/list_logic.dart
@@ -20,7 +20,6 @@ class ListLogic extends GetxController {
     var stockStream = firestore.collection('stock').snapshots();
     // var sub =
     stockStream.listen((event) async {
-      var stocks = <Stock>[];
       for (var doc in event.docs) {
         String dateString = doc.get('date');
         var docId = doc.id;
@@ -28,37 +27,41 @@ class ListLogic extends GetxController {
             firestore.collection('stock/$docId/stock');
         var stockConnectionInSpecificDateResult =
             await stockConnectionInSpecificDate.get();
-        bool crash_finish =
-            stockConnectionInSpecificDateResult.docs.first.get('crash_finish');
-        double current_lower_price = stockConnectionInSpecificDateResult
-            .docs.first
-            .get('current_lower_price');
-        Timestamp current_lower_price_date = stockConnectionInSpecificDateResult
-            .docs.first
-            .get('current_lower_price_date');
-        Timestamp enter_crash_date = stockConnectionInSpecificDateResult
-            .docs.first
-            .get('enter_crash_date');
-        double enter_crash_price = stockConnectionInSpecificDateResult
-            .docs.first
-            .get('enter_crash_price');
-        double high_ref_price = stockConnectionInSpecificDateResult.docs.first
-            .get('high_ref_price');
-        Timestamp high_ref_price_date = stockConnectionInSpecificDateResult
-            .docs.first
-            .get('high_ref_price_date');
-        String name =
-            stockConnectionInSpecificDateResult.docs.first.get('name');
-        var stock = Stock(
-            crash_finish,
-            current_lower_price,
-            current_lower_price_date,
-            enter_crash_date,
-            enter_crash_price,
-            high_ref_price,
-            high_ref_price_date,
-            name);
-        stocks.add(stock);
+        var stocks = <Stock>[];
+        for (var stockConnectionInSpecificDateResultDoc
+            in stockConnectionInSpecificDateResult.docs) {
+          bool crash_finish =
+              stockConnectionInSpecificDateResultDoc.get('crash_finish');
+          double current_lower_price =
+              stockConnectionInSpecificDateResultDoc.get('current_lower_price');
+          Timestamp current_lower_price_date =
+              stockConnectionInSpecificDateResultDoc
+                  .get('current_lower_price_date');
+          Timestamp enter_crash_date =
+              stockConnectionInSpecificDateResultDoc.get('enter_crash_date');
+          double enter_crash_price =
+              stockConnectionInSpecificDateResultDoc.get('enter_crash_price');
+          double high_ref_price =
+              stockConnectionInSpecificDateResultDoc.get('high_ref_price');
+          Timestamp high_ref_price_date =
+              stockConnectionInSpecificDateResultDoc.get('high_ref_price_date');
+          String name = stockConnectionInSpecificDateResultDoc.get('name');
+          var stock = Stock(
+              crash_finish,
+              current_lower_price,
+              current_lower_price_date,
+              enter_crash_date,
+              enter_crash_price,
+              high_ref_price,
+              high_ref_price_date,
+              name);
+          stocks.add(stock);
+        }
+        printInfo(info: '');
+
+        /*var stockConnectionInSpecificDateResultDoc =
+            stockConnectionInSpecificDateResult.docs.first;*/
+
       }
 
       // event.docs.map((QueryDocumentSnapshot<Map<String, dynamic>> doc) {});
