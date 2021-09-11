39a23fdde3a28eb77fc49ca6af059131789c0bcd
Leung Po Ming <>
2021-09-11 12:14:09 +0800

    remove unuse caode

--- lib/page/list/list_logic.dart
+++ lib/page/list/list_logic.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
-import 'dart:ffi';
-import 'dart:ui';
+
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:day_day_3000_fluter_client/obj.dart';
+
 import 'package:day_day_3000_fluter_client/util.dart';
 import 'package:get/get.dart';
 
@@ -33,53 +32,16 @@ class ListLogic extends GetxController {
         var docId = doc.id;
         docIds.add(docId);
         dates.add(dateString);
-        /*var stockConnectionInSpecificDate =
-            firestore.collection('stock/$docId/stock');
-        var stockConnectionInSpecificDateResult =
-            await stockConnectionInSpecificDate.get();
-        List<Stock> stocks = <Stock>[];
-        for (var stockConnectionInSpecificDateResultDoc
-            in stockConnectionInSpecificDateResult.docs) {
-          bool crash_finish =
-              stockConnectionInSpecificDateResultDoc.get('crash_finish');
-          double current_lower_price =
-              stockConnectionInSpecificDateResultDoc.get('current_lower_price');
-          Timestamp current_lower_price_date =
-              stockConnectionInSpecificDateResultDoc
-                  .get('current_lower_price_date');
-          Timestamp enter_crash_date =
-              stockConnectionInSpecificDateResultDoc.get('enter_crash_date');
-          double enter_crash_price =
-              stockConnectionInSpecificDateResultDoc.get('enter_crash_price');
-          double high_ref_price =
-              stockConnectionInSpecificDateResultDoc.get('high_ref_price');
-          Timestamp high_ref_price_date =
-              stockConnectionInSpecificDateResultDoc.get('high_ref_price_date');
-          String name = stockConnectionInSpecificDateResultDoc.get('name');
-          var stock = Stock(
-              crash_finish,
-              current_lower_price,
-              current_lower_price_date,
-              enter_crash_date,
-              enter_crash_price,
-              high_ref_price,
-              high_ref_price_date,
-              name);
-          stocks.add(stock);
-        }
-        dateStocksMap[dateString] = stocks;*/
 
-        /*var stockConnectionInSpecificDateResultDoc =
-            stockConnectionInSpecificDateResult.docs.first;*/
 
       }
 
-      // state.dateStocksMap = dateStocksMap;
+
       state.update((ListState? val) {
         val?.docIds = docIds;
         val?.dates = dates;
       });
-      // event.docs.map((QueryDocumentSnapshot<Map<String, dynamic>> doc) {});
+
     }, onError: (e) {
       showDialog(e.toString(), 'err'.tr);
     });
